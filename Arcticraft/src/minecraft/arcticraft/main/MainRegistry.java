3c00568e79011fe727324ba19948d6ced62164f1
BenDonnelly <Bendonnelly4@gmail.com>
2013-05-25 19:24:32 -1200

    Update MainRegistry.java

--- Arcticraft/src/minecraft/arcticraft/main/MainRegistry.java
+++ Arcticraft/src/minecraft/arcticraft/main/MainRegistry.java
@@ -113,7 +113,7 @@ public class MainRegistry
 	@Instance("AC")
 	public static MainRegistry instance = new MainRegistry();
 	private AC_GuiHandler guiHandler = new AC_GuiHandler();
-	
+	//this is a change in the same class
 	@SidedProxy(clientSide = "arcticraft.main.AC_ClientProxy", serverSide = "arcticraft.main.AC_CommonProxy")
 	public static AC_CommonProxy proxy;
 	public static int dimension = DimensionManager.getNextFreeDimId();
