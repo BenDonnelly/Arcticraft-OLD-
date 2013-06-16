47b02d846a89a0885f704c2c853ed5a5ee5afe2b
BenDonnelly <Bendonnelly4@gmail.com>
2013-06-15 16:47:47 -1200

    Fixed Boar death sound

--- Arcticraft/src/minecraft/arcticraft/main/AC_EventSoundLoad.java
+++ Arcticraft/src/minecraft/arcticraft/main/AC_EventSoundLoad.java
@@ -1,7 +1,10 @@
-package arcticraft.main;                                                             
+package arcticraft.main;
+
 import net.minecraftforge.client.event.sound.SoundLoadEvent;
 import net.minecraftforge.event.ForgeSubscribe;
-import arcticraft.main.*;;
+import arcticraft.main.*;
+
+;
 
 public class AC_EventSoundLoad
 {
@@ -23,20 +26,20 @@ public class AC_EventSoundLoad
 			To play it in the entity file you just use modname.mobsound
 			
 			*/
-			
+
 			//Portal
 			event.manager.soundPoolSounds.addSound("arcticraft/portal.wav", MainRegistry.class.getResource("/mods/AC/sounds/other/portal.wav"));
-			
+
 			//Penguin
 			event.manager.soundPoolSounds.addSound("arcticraft/penguinHurt.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/penguinhurt.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/penguinDeath.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/penguindeath.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/penguinIdle.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/penguinidle.wav"));
-			
+
 			//Pirate
 			event.manager.soundPoolSounds.addSound("arcticraft/pirateHurt.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/buccaneerhit.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/pirateDeath.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/buccaneerdie.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/pirateIdle.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/buccaneermoan.wav"));
-			
+
 			//Polar Bear
 			event.manager.soundPoolSounds.addSound("arcticraft/bearIdle1.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/bear1.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/bearIdle2.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/bear2.wav"));
@@ -45,13 +48,13 @@ public class AC_EventSoundLoad
 			event.manager.soundPoolSounds.addSound("arcticraft/bearHurt1.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/bearHurt1.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/bearHurt2.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/bearHurt2.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/bearHurt3.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/bearHurt3.wav"));
-			
+
 			//Boar
 			event.manager.soundPoolSounds.addSound("arcticraft/boar_hurt.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/boar_hurt.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/boar_living.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/boar_living.wav"));
 			event.manager.soundPoolSounds.addSound("arcticraft/boar_death.wav", MainRegistry.class.getResource("/mods/AC/sounds/mobs/boar_death.wav"));
-			
-		} 
+
+		}
 
 		catch (Exception e)
 		{
