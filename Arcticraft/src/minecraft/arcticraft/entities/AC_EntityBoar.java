0bc851ce75b624b9eb6041948220d8f25a7b6218
BenDonnelly <Bendonnelly4@gmail.com>
2013-07-22 18:15:32 -1200

    Boar changes

--- Arcticraft/src/minecraft/arcticraft/entities/AC_EntityBoar.java
+++ Arcticraft/src/minecraft/arcticraft/entities/AC_EntityBoar.java
@@ -1,46 +1,34 @@
-package arcticraft.entities;
+package net.minecraft.entity.passive;
 
 import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.ai.EntityAIControlledByPlayer;
+import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIFollowParent;
 import net.minecraft.entity.ai.EntityAILookIdle;
 import net.minecraft.entity.ai.EntityAIMate;
 import net.minecraft.entity.ai.EntityAIPanic;
 import net.minecraft.entity.ai.EntityAISwimming;
-import net.minecraft.entity.ai.EntityAITempt;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
-import net.minecraft.entity.effect.EntityLightningBolt;
-import net.minecraft.entity.monster.EntityPigZombie;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.stats.AchievementList;
 import net.minecraft.world.World;
 import arcticraft.items.AC_Item;
 
 public class AC_EntityBoar extends EntityAnimal
 {
 
-	/** AI task for player control. */
-	private final EntityAIControlledByPlayer aiControlledByPlayer;
-
 	public AC_EntityBoar(World par1World)
 	{
 		super(par1World);
-		this.setSize(2.0F, 1.8F);
+		this.setSize(0.9F, 0.9F);
 		this.getNavigator().setAvoidsWater(true);
-		float f = 0.25F;
 		this.tasks.addTask(0, new EntityAISwimming(this));
-		this.tasks.addTask(1, new EntityAIPanic(this, 0.38F));
-		this.tasks.addTask(2, this.aiControlledByPlayer = new EntityAIControlledByPlayer(this, 0.34F));
-		this.tasks.addTask(3, new EntityAIMate(this, f));
-		this.tasks.addTask(4, new EntityAITempt(this, 0.3F, Item.carrotOnAStick.itemID, false));
-		this.tasks.addTask(4, new EntityAITempt(this, 0.3F, Item.carrot.itemID, false));
-		this.tasks.addTask(5, new EntityAIFollowParent(this, 0.28F));
-		this.tasks.addTask(6, new EntityAIWander(this, f));
+		this.tasks.addTask(1, new EntityAIPanic(this, 1.25D));
+		this.tasks.addTask(3, new EntityAIMate(this, 1.0D));
+		this.tasks.addTask(5, new EntityAIFollowParent(this, 1.1D));
+		this.tasks.addTask(6, new EntityAIWander(this, 1.0D));
 		this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityPlayer.class, 6.0F));
 		this.tasks.addTask(8, new EntityAILookIdle(this));
 	}
@@ -53,9 +41,11 @@ public class AC_EntityBoar extends EntityAnimal
 		return true;
 	}
 
-	public int getMaxHealth()
+	protected void func_110147_ax()
 	{
-		return 10;
+		super.func_110147_ax();
+		this.func_110148_a(SharedMonsterAttributes.field_111267_a).func_111128_a(15.0D);
+		this.func_110148_a(SharedMonsterAttributes.field_111263_d).func_111128_a(0.25D);
 	}
 
 	protected void updateAITasks()
@@ -63,39 +53,10 @@ public class AC_EntityBoar extends EntityAnimal
 		super.updateAITasks();
 	}
 
-	/**
-	 * returns true if all the conditions for steering the entity are met. For
-	 * pigs, this is true if it is being ridden by a player and the player is
-	 * holding a carrot-on-a-stick
-	 */
-	public boolean canBeSteered()
-	{
-		ItemStack itemstack = ((EntityPlayer) this.riddenByEntity).getHeldItem();
-		return itemstack != null;
-	}
-
 	protected void entityInit()
 	{
 		super.entityInit();
 		this.dataWatcher.addObject(16, Byte.valueOf((byte) 0));
-	}
-
-	/**
-	 * (abstract) Protected helper method to write subclass entity data to NBT.
-	 */
-	public void writeEntityToNBT(NBTTagCompound par1NBTTagCompound)
-	{
-		super.writeEntityToNBT(par1NBTTagCompound);
-		par1NBTTagCompound.setBoolean("Saddle", this.getSaddled());
-	}
-
-	/**
-	 * (abstract) Protected helper method to read subclass entity data from NBT.
-	 */
-	public void readEntityFromNBT(NBTTagCompound par1NBTTagCompound)
-	{
-		super.readEntityFromNBT(par1NBTTagCompound);
-		this.setSaddled(par1NBTTagCompound.getBoolean("Saddle"));
 	}
 
 	/**
@@ -131,38 +92,16 @@ public class AC_EntityBoar extends EntityAnimal
 	}
 
 	/**
-	 * Called when a player interacts with a mob. e.g. gets milk from a cow,
-	 * gets into the saddle on a pig.
-	 */
-	public boolean interact(EntityPlayer par1EntityPlayer)
-	{
-		if(super.interact(par1EntityPlayer))
-		{
-			return true;
-		}
-		else if(this.getSaddled() && ! this.worldObj.isRemote && (this.riddenByEntity == null || this.riddenByEntity == par1EntityPlayer))
-		{
-			par1EntityPlayer.mountEntity(this);
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	/**
 	 * Returns the item ID for the item the mob drops on death.
 	 */
 	protected int getDropItemId()
 	{
-		return this.isBurning() ? Item.porkCooked.itemID : Item.porkRaw.itemID;
+		return this.isBurning() ? AC_Item.boarMeat.itemID : AC_Item.uncookedBoarMeat.itemID;
 	}
 
 	/**
-	 * Drop 0-2 items of this living's type. @param par1 - Whether this entity
-	 * has recently been hit by a player. @param par2 - Level of Looting used to
-	 * kill this mob.
+	 * Drop 0-2 items of this living's type. @param par1 - Whether this entity has recently been hit by a player. @param
+	 * par2 - Level of Looting used to kill this mob.
 	 */
 	protected void dropFewItems(boolean par1, int par2)
 	{
@@ -180,65 +119,10 @@ public class AC_EntityBoar extends EntityAnimal
 			}
 		}
 
-		if(this.getSaddled())
-		{
-			this.dropItem(Item.saddle.itemID, 1);
-		}
 	}
 
 	/**
-	 * Returns true if the pig is saddled.
-	 */
-	public boolean getSaddled()
-	{
-		return (this.dataWatcher.getWatchableObjectByte(16) & 1) != 0;
-	}
-
-	/**
-	 * Set or remove the saddle of the pig.
-	 */
-	public void setSaddled(boolean par1)
-	{
-		if(par1)
-		{
-			this.dataWatcher.updateObject(16, Byte.valueOf((byte) 1));
-		}
-		else
-		{
-			this.dataWatcher.updateObject(16, Byte.valueOf((byte) 0));
-		}
-	}
-
-	/**
-	 * Called when a lightning bolt hits the entity.
-	 */
-	public void onStruckByLightning(EntityLightningBolt par1EntityLightningBolt)
-	{
-		if(! this.worldObj.isRemote)
-		{
-			EntityPigZombie entitypigzombie = new EntityPigZombie(this.worldObj);
-			entitypigzombie.setLocationAndAngles(this.posX, this.posY, this.posZ, this.rotationYaw, this.rotationPitch);
-			this.worldObj.spawnEntityInWorld(entitypigzombie);
-			this.setDead();
-		}
-	}
-
-	/**
-	 * Called when the mob is falling. Calculates and applies fall damage.
-	 */
-	protected void fall(float par1)
-	{
-		super.fall(par1);
-
-		if(par1 > 5.0F && this.riddenByEntity instanceof EntityPlayer)
-		{
-			((EntityPlayer) this.riddenByEntity).triggerAchievement(AchievementList.flyPig);
-		}
-	}
-
-	/**
-	 * This function is used when two same-species animals in 'love mode' breed
-	 * to generate the new baby animal.
+	 * This function is used when two same-species animals in 'love mode' breed to generate the new baby animal.
 	 */
 	public AC_EntityBoar spawnBabyAnimal(EntityAgeable par1EntityAgeable)
 	{
@@ -246,20 +130,12 @@ public class AC_EntityBoar extends EntityAnimal
 	}
 
 	/**
-	 * Checks if the parameter is an item which this animal can be fed to breed
-	 * it (wheat, carrots or seeds depending on the animal type)
+	 * Checks if the parameter is an item which this animal can be fed to breed it (wheat, carrots or seeds depending on
+	 * the animal type)
 	 */
 	public boolean isBreedingItem(ItemStack par1ItemStack)
 	{
 		return par1ItemStack != null && par1ItemStack.itemID == Item.carrot.itemID;
-	}
-
-	/**
-	 * Return the AI task for player control.
-	 */
-	public EntityAIControlledByPlayer getAIControlledByPlayer()
-	{
-		return this.aiControlledByPlayer;
 	}
 
 	public EntityAgeable createChild(EntityAgeable par1EntityAgeable)
