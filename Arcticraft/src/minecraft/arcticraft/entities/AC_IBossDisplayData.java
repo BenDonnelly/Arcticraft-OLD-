b39870d8ebe6f2c8d974c40b9e9b5c1e9672cbc2
Lolcroc <meneertjeyves@gmail.com>
2013-07-25 22:49:29 +0200

    Stuff for game overlays (in progress)
    Not done yet. small changes

--- Arcticraft/src/minecraft/arcticraft/entities/AC_IBossDisplayData.java
+++ Arcticraft/src/minecraft/arcticraft/entities/AC_IBossDisplayData.java
@@ -1,23 +1,9 @@
 package arcticraft.entities;
 
-public interface AC_IBossDisplayData
+import net.minecraft.entity.boss.IBossDisplayData;
+
+public interface AC_IBossDisplayData extends IBossDisplayData
 {
-
-	/**
-	 * Max health of the boss.
-	 */
-
-	float func_110138_aP();
-
-	/**
-	 * Returns the health points of the boss.
-	 */
-	float func_110143_aJ();
-
-	/**
-	 * Gets the username of the entity.
-	 */
-	String getEntityName();
 
 	boolean isMiniBoss();
 }
