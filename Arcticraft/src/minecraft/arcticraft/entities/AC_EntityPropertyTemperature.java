21b9f2788e81e9143229f39b8b7c51c5a0d8d152
Lolcroc <meneertjeyves@gmail.com>
2013-08-15 15:27:29 +0200

    Temperature stuff WIP
    wip

---
+++ Arcticraft/src/minecraft/arcticraft/entities/AC_EntityPropertyTemperature.java
@@ -0,0 +1,40 @@
+package arcticraft.entities;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IExtendedEntityProperties;
+
+public class AC_EntityPropertyTemperature implements IExtendedEntityProperties {
+	
+	private EntityPlayer player;
+	private static final int tempID = 19;
+	public static final int maxTemp = 100;
+	
+	@Override
+	public void saveNBTData(NBTTagCompound compound) {
+		compound.setInteger("ACTemperature", this.getTemperature());
+	}
+
+	@Override
+	public void loadNBTData(NBTTagCompound compound) {
+		this.setTemperature(compound.getInteger("ACTemperature"));
+	}
+
+	@Override
+	public void init(Entity entity, World world) {
+		entity.getDataWatcher().addObject(tempID, Integer.valueOf(50));
+		
+		this.player = (EntityPlayer) entity;
+	}
+	
+	public int getTemperature() {
+		return this.player.getDataWatcher().getWatchableObjectInt(tempID);
+	}
+	
+	public void setTemperature(int temp) {
+		this.player.getDataWatcher().updateObject(tempID, Integer.valueOf(temp));
+	}
+
+}
